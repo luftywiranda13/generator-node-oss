fdfd80fd2ce8014d62cba034b45186d1184cfd8c
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-08-31 18:36:11 +0700

    refactor: use correct flag to silent `git init`

--- app/index.js
+++ app/index.js
@@ -142,7 +142,7 @@ module.exports = class extends Generator {
       );
     }
 
-    this.spawnCommandSync('git', ['init'], { stdio: false });
+    this.spawnCommandSync('git', ['init', '--quiet']);
   }
   install() {
     const hasYarn = commandExists.sync('yarn');
