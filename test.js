e4e91bf81aec5a08a61cceb5454cf5a24a4ac70e
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-02 22:03:23 +0700

    feat(prettier): make CI build exit non-zero if code styles don't match with `prettier` (#59)
    * feat(prettier): make CI build exit non-zero if code styles don't match with `prettier`
    
    * chore(prettier): add `package-lock.json` to ignored list
    
    * chore(prettier): remove `node_modules` from ignored list
    
    `node_modules` is already ignored by default

--- test.js
+++ test.js
@@ -163,6 +163,10 @@ describe('prompts', () => {
         .withPrompts({ extras: ['prettier'] })
         .then(() => {
           assert.fileContent('package.json', 'eslint-config-prettier');
+          assert.fileContent(
+            '.travis.yml',
+            'node node_modules/.bin/prettier -l "*.{js,json,md}"'
+          );
           assert.file('.prettierignore');
           assert.jsonFileContent('package.json', {
             'lint-staged': {
